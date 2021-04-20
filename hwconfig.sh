5d4689420798fb527c9c0f732191af05fc8bcaba
TheRaptors <Mcsys@hotmail.com>
2021-04-20 11:56:57 +0800

    新增 hwconfig.pl 及用法

--- hwconfig.sh
+++ hwconfig.sh
@@ -8,8 +8,19 @@
 
 BasePWD=$(pwd)
 
+export PATH=$(pwd)/bin/:$PATH
+
 chmod +x ./bin/*
 
-./bin/hwconfig -r
+hwconfig -r
 
 rm -rf MegaSAS.log ./var/*
+
+echo ""
+echo "hwconfig 用法:"
+echo '当前 SSH Terminal 执行命令: export PATH=$(pwd)/bin/:$PATH'
+hwconfig -h
+
+echo ""
+echo "或使用另一个脚本(可能会报错):"
+echo "perl hwconfig.pl"
